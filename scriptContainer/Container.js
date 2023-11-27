b9090830055f4076ad92daec235c63ed06e75f21
Unk0wn1337 <116260492+Unk0wn1337@users.noreply.github.com>
2023-11-27 13:03:32 +0100

    mappak rendezve

--- scriptContainer/Container.js
+++ scriptContainer/Container.js
@@ -1,7 +1,7 @@
 export function homePage(){
     let homePage = $("#szamalkHomePage");
      $(homePage).on("click",function() {
-        window.location.href = "../index.html";
+        window.location.href = "../htmlPages/index.html";
      });
 }
 
